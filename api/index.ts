643238939fcb0b025b2ec0872f82be59f67846d5
Dawid Derewońko <angeldeabyssi@gmail.com>
2023-10-05 06:19:47 +0200

    mongoose configuration

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+// @ts-ignore
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected to MongoDB")
+}).catch(error => {
+    console.log(error)
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
